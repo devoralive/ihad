d5118530532fa20604d13e3fc40bf28f7b5f1256
Nicolas Dufreche <dious@pm.me>
2018-08-06 23:41:10 +0200

    first strike

--- ihad/ViewController.swift
+++ ihad/ViewController.swift
@@ -7,19 +7,30 @@
 //
 
 import UIKit
+import WebKit
 
 class ViewController: UIViewController {
+    
+    var webView:WKWebView!
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+        let url:URL = URL(string: "http://www.radio-ihaveadream.com/box-radio")!
+        let request:URLRequest = URLRequest(url: url)
+        
+        webView = WKWebView()
+        webView.load(request)
+        webView.frame = CGRect(x: 0, y: 0, width: UIScreen.main.bounds.width, height: UIScreen.main.bounds.height)
+        
+        view.addSubview(webView)
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
 
 }
 
